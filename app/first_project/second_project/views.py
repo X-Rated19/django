3abdced63774bc6708a7c7b9447d606888de1032
xrated887 <atabekov.daulet.kz@gmail.com>
2021-04-22 19:17:34 +0600

    New commit2

--- app/first_project/second_project/views.py
+++ app/first_project/second_project/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Post
+from .models import Post, Tag
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all()
@@ -9,3 +9,11 @@ def posts_list(request):
 def post_detail(request, slug):
     post = Post.objects.get(slug__iexact=slug)
     return render(request, 'second_project/post_detail.html', context={'post': post})
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'second_project/tags_list.html', context={'tags': tags})
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'second_project/tag_detail.html', context={'tag': tag})
